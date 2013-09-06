5b3883d0b45a0f6c9dfb97c6c7ccf45f8225060c
Kevin Ko <enhasa@gmail.com>
2013-09-06 16:34:39 -0700

    Add base routes/views

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
+    if current_user
+      @user = User.find(current_user.id)
+    else
+      @user = User.new
+    end
+
+    @shows = @user.shows
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Hedonismbot::Application.routes.draw do
   root :to => 'static_pages#index'
+  resources :users, except: [:show, :index]
+  resources :shows, only: [:create, :destroy]
+  resources :trackings, only: [:create, :destroy]
+  get '/about' => "static_pages#about", as: "about"
+  get '/signup' => "users#new", as: "signup"
+  get '/signup' => "sessions#new", as: "login"
 end
