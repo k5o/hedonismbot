47ec46837279fd22edc020d338ea3910a33b100a
Kevin Ko <enhasa@gmail.com>
2013-09-06 13:41:48 -0700

    Initial commit
    I apologize for nothing.

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'pg'
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'debugger'
+gem 'tvdb_party'
+gem 'crack'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hedonismbot::Application.config.secret_token = 'c056ae76b567166d432b830f24db8f3d4d2b9cbe1525d2c2278fb836d0746787990620cabf47c5bc495eb67f602b52b5ab76b5c02dedc1d172d4e8e4a7dc9f02'
