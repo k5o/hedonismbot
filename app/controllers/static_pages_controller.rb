a289541a0f2a940d99e8ec7f9ca2a7dad01b6f9c
Kevin Ko <enhasa@gmail.com>
2013-09-07 11:14:50 -0700

    Add new demo shows, fix main page css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,12 @@ class StaticPagesController < ApplicationController
     end
 
     @shows = @user.shows
-    @demo_shows = [Show.find_by_title("Game of Thrones"), Show.find_by_title("Futurama")]
+
+    @demo_shows = [Show.find_by_title("Game of Thrones"), 
+      Show.find_by_title("Futurama"),
+      Show.find_by_title("Adventure Time"),
+      Show.find_by_title("Parks and Recreation"),
+      Show.find_by_title("Suits"),
+      Show.find_by_title("Top Gear")]
   end
 end
