6ac669846e8ad8952ea1680a6c1895a93541a981
Kevin Ko <enhasa@gmail.com>
2013-09-07 10:43:09 -0700

    Correct variables in show fetch, added new seeds

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,36 +16,38 @@ class Show < ActiveRecord::Base
     end
 
     def create_show_data(query)
-      response = HTTParty.get('http://services.tvrage.com/tools/quickinfo.php', :query => {:show => query}, :format => :html)
+      response = HTTParty.get('http://services.tvrage.com/tools/quickinfo.php', :query => {:show => query}, :format => :html) 
       show_data = Crack::XML.parse(response)["pre"]
 
       raise ShowNotFound unless show_data
 
+      title = "" ; latest_episode = "" ; next_episode = "" ; status = "" ; airtime = ""
+
       show_data.split("\n").each do |data|
-        @title = data if data[/^Show Name/]
-        @latest_episode = data if data[/^Latest Episode/]
-        @next_episode = data if data[/^Next Episode/]
-        @status = data if data[/^Status/]
-        @airtime = data if data[/^Airtime/]
+        title          = data if data[/^Show Name/]
+        latest_episode = data if data[/^Latest Episode/]
+        next_episode   = data if data[/^Next Episode/]
+        status         = data if data[/^Status/]
+        airtime        = data if data[/^Airtime/]
       end
 
-      @title = @title.match(/@(.+)$/).captures.first
+      title = title.match(/@(.+)$/).captures.first unless title.blank?
 
       tvdb = TvdbParty::Search.new(ENV["TVDB_API_KEY"])
-      show = tvdb.get_series_by_id(tvdb.search(@title).first["seriesid"])
-      @banner = show.series_banners('en').first.url
+      show = tvdb.get_series_by_id(tvdb.search(title).first["seriesid"])
+      banner = show.series_banners('en').first.url
 
       Show.create!({ 
-        title: @title,
-        last_episode: @latest_episode[/(\d+x\d+)/],
-        last_title: @latest_episode.match(/\^(.+)\^/).captures.first,
-        last_airdate: @latest_episode.match(/\^(\D{3}\/\d{2}\/\d{4})$/).captures.first,
-        next_episode: @next_episode ? @next_episode[/(\d+x\d+)/] : nil,
-        next_title: @next_episode ? @next_episode.match(/\^(.+)\^/).captures.first : nil,
-        next_airdate: @next_episode ? @next_episode.match(/\^(\D{3}\/\d{2}\/\d{4})$/).captures.first : nil,
-        status: @status.match(/@(.+)$/).captures.first,
-        airtime: @airtime ? @airtime.match(/at\W(.+)$/).captures.first : nil,
-        banner: @banner
+        title: title,
+        last_episode: latest_episode[/(\d+x\d+)/],
+        last_title: latest_episode.match(/\^(.+)\^/).captures.first,
+        last_airdate: latest_episode.match(/\^(\D{3}\/\d{2}\/\d{4})$/).captures.first,
+        next_episode: next_episode.present? ? next_episode[/(\d+x\d+)/] : nil,
+        next_title: next_episode.present? ? next_episode.match(/\^(.+)\^/).captures.first : nil,
+        next_airdate: next_episode.present? ? next_episode.match(/\^(\D{3}\/\d{2}\/\d{4})$/).captures.first : nil,
+        status: status.match(/@(.+)$/).captures.first,
+        airtime: airtime.present? ? airtime.match(/@(.+)$/).captures.first : nil,
+        banner: banner
       })
     end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
 Show.create_show_data("Game of Thrones")
+sleep 2
 Show.create_show_data("Futurama")
+sleep 2
+Show.create_show_data("Adventure Time")
+sleep 2
+Show.create_show_data("Parks and Recreation")
+sleep 2
+Show.create_show_data("Suits")
+sleep 2
+Show.create_show_data("Wilfred")
+sleep 2
+Show.create_show_data("Workaholics")
