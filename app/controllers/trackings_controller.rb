b2cacfc029889a266028c149f219d67c86fed731
Kevin Ko <enhasa@gmail.com>
2013-09-07 12:13:44 -0700

    Debug user query workflow

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -1,18 +1,36 @@
 class TrackingsController < ApplicationController
+  before_filter :load_imperatives
+
   def create
-    current_user ? @user = User.find(current_user.id) : @user = User.new
-    
-    @title = params[:show_title].titleize
+    @title = params[:show_title]    
 
-    if Tracking.show_available?(@title)
-      canonical_title = Tracking.show_available(@title)
+    if Show.show_available?(@title)
+      @canonical_title = Show.show_available?(@title)
+      @show            = Show.find_or_initialize_by_title(@canonical_title)
 
-      @show = Show.find_or_initialize_by_title(canonical_title)
-
-      Show.create_show_data(canonical_title, @show.id) if @show.new_record?
-
+      Show.create_show_data(@canonical_title, @show.id) if @show.new_record?
       Tracking.create(:user_id => @user.id, :show_id => @show.id)
       
-      # render partial
+      render 'static_pages/index'
+    else
+      flash.now[:notice] = "Show not found!"
+
+      render 'static_pages/index'
+    end
+  end
+
+  private
+
+  def load_imperatives
+    current_user ? @user = User.find(current_user.id) : @user = User.new
+ 
+    @shows = @user.shows
+
+    @demo_shows = [Show.find_by_title("Game of Thrones"), 
+    Show.find_by_title("Futurama"),
+    Show.find_by_title("Adventure Time"),
+    Show.find_by_title("Parks and Recreation"),
+    Show.find_by_title("Suits"),
+    Show.find_by_title("Top Gear")]
   end
 end
