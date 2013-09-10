9cfcd82969b1c49dbba77885bca7b6a5c06b5aab
Kevin Ko <enhasa@gmail.com>
2013-09-09 17:47:14 -0700

    Add ajax loader

--- app/assets/javascripts/trackings.js
+++ app/assets/javascripts/trackings.js
@@ -1,11 +1,16 @@
 var Tracking = {
   init: function() {
     $('.delete').on('ajax:success', 'a.delete_tracking', this.deleteTracking);
+    $('#query_form_tag').on('submit', this.invokeAjaxLoader);
   },
 
   deleteTracking: function() {
     $(this).parent().parent().fadeOut(400);
   },
+
+  invokeAjaxLoader: function() {
+    $('#query_submit').html("<img src='/assets/ajax-loader.gif'>")
+  }
 }
 
 $(document).ready(function() {
