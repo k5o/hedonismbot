e8c95d4cd2fb2f67ec7d3e9e937bde294b22eba1
Kevin Ko <enhasa@gmail.com>
2013-09-10 21:31:08 -0700

    Add presentation helpers for show cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def date_presenter_for_next_airdate(date)
+    if date.nil?
+      "N/A"
+    elsif (date - Date.today) == 0
+      "Today"
+    elsif (date - Date.today) == 1
+      "Tomorrow"
+    else
+      date.strftime("%m-%d-%Y")
+    end
+  end
+
+  def date_presenter_for_last_airdate(date)
+    date ? date.strftime("%m-%d-%Y") : "Show in Development"
+  end
 end
