62d13f51ee3a9528205fef984670e0c4b5c92e9e
Kevin Ko <enhasa@gmail.com>
2013-10-07 02:10:32 -0700

    Add resurrection route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Hedonismbot::Application.routes.draw do
   root to: 'static_pages#index'
   resources :users, except: [:show, :index]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :shows, only: [:create, :destroy]
+  resources :shows, only: [:index]
   resources :trackings, only: [:create, :destroy]
   get '/about' => "static_pages#about", as: "about"
   get '/signup' => "users#new", as: "signup"
   get '/signup' => "sessions#new", as: "login"
   get '/logout' => "sessions#destroy", as: "logout"
+  post 'shows/:id/resurrect', to: "shows#resurrect", as: "resurrect"
 end
